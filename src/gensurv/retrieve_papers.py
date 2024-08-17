af3ee16f8828ebcc16370064869c4c5113f9fc5d
roy29fuku <roy29fuku@gmail.com>
2024-08-17 15:02:30 +0900

    Add function to fetch papers using given title as direct query

--- src/gensurv/retrieve_papers.py
+++ src/gensurv/retrieve_papers.py
@@ -1,4 +1,14 @@
+from retrievers.semantic_scholar import SemanticScholarRetriever
 
 
 def retrieve_papers(title: str) -> list[str]:
-    ...
+    """
+
+    :param title: A title of a paper which you want to generate an overview for.
+    :return:
+    """
+    retriever = SemanticScholarRetriever()
+    query = title  # TODO: convert title to query
+    docs = retriever.invoke(query)
+    papers = [doc.metadata for doc in docs]
+    return papers
