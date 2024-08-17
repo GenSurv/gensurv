0aeda04e4473621f55f76db9c848bbd7907381fc
roy29fuku <roy29fuku@gmail.com>
2024-08-17 14:50:04 +0900

    関数のガワのみ作成

---
+++ src/gensurv/__init__.py
@@ -0,0 +1,6 @@
+from .retrieve_papers import retrieve_papers
+from .generate_headings import generate_headings
+from .classify_papers import classify_papers
+from .generate_overview import generate_overview
+from .generate_draft import generate_draft
+from .utils import load_papers, load_headings
---
+++ src/gensurv/generate_draft.py
@@ -0,0 +1,4 @@
+
+
+def generate_draft(overview: dict[str, str]) -> str:
+    ...
